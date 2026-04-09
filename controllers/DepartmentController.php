1a9dd30f9a284fd78e11df205c5675e2d95e9304
Thabo <stylesbylethabo@gmail.com>
2026-04-09 11:10:44 +0200

    feat(department): implemented the department controller

---
+++ controllers/DepartmentController.php
@@ -0,0 +1,49 @@
+<?php
+class DepartmentController extends controller {
+pubpic function index() {
+    $this->checkRole('admin');
+    $departmentModel = new Department();
+    $departments = $departmentModel->getAllDepartments();
+    $this->render('departments/index', ['departments' => $departments]);
+}
+
+public function create() {
+
+$this->checkRole('admin');
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $data = [
+        'name' => $_POST['name'],
+        'description' => $_POST['description']
+    ];
+    $departmentModel = new Department();
+    if ($departmentModel->create($data)) {
+        $this->redirect('/departments');
+    }  
+}
+$this->render('departments/create');
+}
+
+public function edit($id) {
+    $this->checkRole('admin');
+  $departmentModel = new Department();
+    $department = $departmentModel->getById($id);
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $data = [
+            'name' => $_POST['name'],
+            'description' => $_POST['description']
+        ];
+        if ($departmentModel->update($id, $data)) {
+            $this->redirect('/departments');
+        }
+    }
+    $this->render('departments/edit', ['department' => $department]);
+}
+
+public function delete($id) {
+    $this->checkRole('admin');
+    $departmentModel = new Department();
+    if ($departmentModel->delete($id)) {
+        $this->redirect('/departments');
+    }   
+}
+?>
